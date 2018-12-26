518a4e1b917f8f0d6071e0f1b81abaa16c8a1e0d
iixixv <iixixv.me@gmail.com>
2018-12-27 00:55:01 +0200

    list viewer repos

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
-import { View, Text, ActivityIndicator, Button } from 'react-native'
+import { View, Text, ActivityIndicator, Button, FlatList } from 'react-native'
+import { List, ListItem, Body, Right, Icon } from 'native-base'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
@@ -10,6 +13,24 @@ const VIEWER = gql`
 	query {
 		viewer {
 			login
+			repositories(last: 20) {
+				edges {
+					node {
+						id
+						name
+						description
+						createdAt
+						primaryLanguage {
+							name
+						}
+						refs(first: 10, refPrefix: "refs/heads/") {
+							nodes {
+								name
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 `
@@ -22,27 +43,44 @@ export default class Home extends React.Component {
 					if (loading) return <ActivityIndicator size="small" />
 					if (!data) goLogin()
 
-					return (
-						<View style={{ flex: 1, justifyContent: 'center' }}>
-							{data && data.viewer && data.viewer.login ? (
-								<Text>Welcome {data.viewer.login}</Text>
-							) : (
-								<Text />
-							)}
-							<Button
-								title="Logout"
-								onPress={async () => {
-									try {
-										await removeToken()
-										client.resetStore()
-										goLogin()
-									} catch (e) {
-										console.log(e)
-									}
-								}}
-							/>
-						</View>
-					)
+					if (data)
+						return (
+							<View style={{ flex: 1, justifyContent: 'center' }}>
+								<FlatList
+									data={data.viewer.repositories.edges}
+									renderItem={({ item }: any) => (
+										<ListItem onPress={() => {}}>
+											<Body>
+												<Text>{item.node.name}</Text>
+											</Body>
+											<Right>
+												<Text />
+												{/* <Ionicons name="ios-add" /> */}
+												{/* <Icon
+													name="arrow-forward"
+													// size={20}
+												/> */}
+											</Right>
+										</ListItem>
+									)}
+									keyExtractor={(item: any) => item.node.id}
+								/>
+
+								{/* <Button
+									title="Logout"
+									onPress={async () => {
+										try {
+											await removeToken()
+											client.resetStore()
+											goLogin()
+										} catch (e) {
+											console.log(e)
+										}
+									}}
+								/> */}
+							</View>
+						)
+					return null
 				}}
 			</Query>
 		)
