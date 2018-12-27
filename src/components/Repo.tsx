c5e5a687e7598cf13610a5c345a95c46fe0dce6d
iixixv <iixixv.me@gmail.com>
2018-12-27 17:00:46 +0200

    add repo screen

---
+++ src/components/Repo.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react'
+import {
+	View,
+	Text,
+	ActivityIndicator,
+	StyleSheet,
+	FlatList
+} from 'react-native'
+
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const GET_REPO = gql`
+	query getReop($owner: String!, $name: String!) {
+		repository(owner: $owner, name: $name) {
+			name
+			description
+			createdAt
+			languages(first: 10) {
+				nodes {
+					id
+					name
+				}
+			}
+		}
+	}
+`
+
+interface IProps {
+	owner: any
+	name: any
+}
+
+export default class Repo extends React.Component<IProps> {
+	render() {
+		const { owner, name } = this.props
+
+		return (
+			<Query query={GET_REPO} variables={{ owner, name }}>
+				{({ loading, data }) => {
+					if (loading)
+						return (
+							<View style={styles.container}>
+								<ActivityIndicator size="large" color="grey" />
+							</View>
+						)
+
+					if (data)
+						return (
+							<View style={styles.container}>
+								<Text>Name: {data.repository.name}</Text>
+								<Text>
+									Description: {data.repository.description}
+								</Text>
+								<Text>
+									Created At: {data.repository.description}
+								</Text>
+								<FlatList
+									data={data.repository.languages.nodes}
+									renderItem={({ item }: any) => (
+										<Text>{item.name}</Text>
+									)}
+									keyExtractor={(item: any) => item.id}
+								/>
+							</View>
+						)
+					return null
+				}}
+			</Query>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+		// backgroundColor: 'lightgrey'
+	}
+})
--- src/navigation/screens.ts
+++ src/navigation/screens.ts
@@ -5,10 +5,12 @@ import Welcome from './Welcome'
 import Login from '../components/Login'
 import Home from '../components/Home'
 import Repos from '../components/Repos'
+import Repo from '../components/Repo'
 
 export function registerScreens() {
 	Navigation.registerComponent('Welcome', () => Welcome)
 	Navigation.registerComponent('Login', () => withProvider(Login))
 	Navigation.registerComponent('Home', () => withProvider(Home))
 	Navigation.registerComponent('Repos', () => withProvider(Repos))
+	Navigation.registerComponent('Repo', () => withProvider(Repo))
 }
