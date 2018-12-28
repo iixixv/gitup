164b3fed0e4e659e65596613d31203c79cf9b6c6
iixixv <iixixv.me@gmail.com>
2018-12-28 02:25:22 +0200

    add info to screens

--- src/navigation/screens.ts
+++ src/navigation/screens.ts
@@ -6,9 +6,11 @@ import Login from '../components/Login'
 import Home from '../components/Home'
 import Repos from '../components/Repos'
 import Repo from '../components/Repo'
+import Info from '../components/Info'
 
 export function registerScreens() {
 	Navigation.registerComponent('Welcome', () => Welcome)
+	Navigation.registerComponent('Info', () => Info)
 	Navigation.registerComponent('Login', () => withProvider(Login))
 	Navigation.registerComponent('Home', () => withProvider(Home))
 	Navigation.registerComponent('Repos', () => withProvider(Repos))
