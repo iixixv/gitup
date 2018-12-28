cc07494d33bd3de707f227b8ac4e7fedc4097f95
iixixv <iixixv.me@gmail.com>
2018-12-28 03:01:19 +0200

    update repo styles

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react'
-import {
-	View,
-	Text,
-	ActivityIndicator,
-	StyleSheet,
-	FlatList
-} from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import { colors } from '../styles/colors'
+import Loader from './Loader'
 
 const GET_REPO = gql`
 	query getReop($owner: String!, $name: String!) {
@@ -17,11 +13,6 @@ const GET_REPO = gql`
 			description
 			createdAt
 			refs(first: 10, refPrefix: "refs/heads/") {
-				nodes {
-					name
-				}
-			}
-			languages(first: 10) {
 				nodes {
 					id
 					name
@@ -43,37 +34,59 @@ export default class Repo extends React.Component<IProps> {
 		return (
 			<Query query={GET_REPO} variables={{ owner, name }}>
 				{({ loading, data }) => {
-					if (loading)
-						return (
-							<View style={styles.container}>
-								<ActivityIndicator size="large" color="grey" />
-							</View>
-						)
+					if (loading) return <Loader />
 
 					if (data)
 						return (
 							<View style={styles.container}>
-								<Text>Name: {data.repository.name}</Text>
-								<Text>
-									Description: {data.repository.description}
+								<Text style={styles.header}>
+									{data.repository.name}
 								</Text>
-								<Text>
-									Created At: {data.repository.description}
-								</Text>
-								<FlatList
-									data={data.repository.refs.nodes}
-									renderItem={({ item }: any) => (
-										<Text>{item.name}</Text>
-									)}
-									keyExtractor={(item: any) => item.id}
-								/>
-								<FlatList
-									data={data.repository.languages.nodes}
-									renderItem={({ item }: any) => (
-										<Text>{item.name}</Text>
-									)}
-									keyExtractor={(item: any) => item.id}
-								/>
+
+								{data.repository.description && (
+									<>
+										<Text style={styles.item}>
+											Description
+										</Text>
+										<Text style={styles.info}>
+											{data.repository.description}
+										</Text>
+									</>
+								)}
+
+								{data.repository.createdAt && (
+									<>
+										<Text style={styles.item}>
+											Created at
+										</Text>
+										<Text style={styles.info}>
+											{data.repository.createdAt}
+										</Text>
+									</>
+								)}
+
+								{data.repository.refs.nodes && (
+									<>
+										<Text style={styles.item}>
+											Branches
+										</Text>
+
+										<FlatList
+											data={data.repository.refs.nodes}
+											renderItem={({ item }: any) => (
+												<Text
+													key={item.id}
+													style={styles.info}
+												>
+													{item.name}
+												</Text>
+											)}
+											keyExtractor={(item: any) =>
+												item.id
+											}
+										/>
+									</>
+								)}
 							</View>
 						)
 					return null
@@ -86,8 +99,26 @@ export default class Repo extends React.Component<IProps> {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-		// backgroundColor: 'lightgrey'
+		padding: 50
+	},
+	header: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: colors.accent,
+		textDecorationLine: 'underline',
+		marginBottom: 30
+	},
+	item: {
+		fontSize: 17,
+		fontWeight: 'bold',
+		color: colors.black,
+		marginTop: 30,
+		marginBottom: 5
+	},
+	info: {
+		fontSize: 14,
+		marginBottom: 5,
+		marginLeft: 40,
+		color: colors.black
 	}
 })
