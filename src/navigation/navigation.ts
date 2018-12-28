e530ac10e96831fea166f8c3fdecd27a98fcb0aa
iixixv <iixixv.me@gmail.com>
2018-12-28 03:00:40 +0200

    update back button color

--- src/navigation/navigation.ts
+++ src/navigation/navigation.ts
@@ -1,10 +1,18 @@
 import { Navigation } from 'react-native-navigation'
+import { colors } from '../styles/colors'
 
 export const goHome = () =>
 	Navigation.setRoot({
 		root: {
 			stack: {
 				id: 'App',
+				options: {
+					topBar: {
+						backButton: {
+							color: colors.accent
+						}
+					}
+				},
 				children: [
 					{
 						component: {
